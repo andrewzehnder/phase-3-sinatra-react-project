768ac57e792ddbb6ec6c4f49ff5a046b3b0a5d4c
Andrew <azehnder2@gmail.com>
2022-09-20 16:03:40 -0700

    Adding controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,34 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+  require 'pry'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  #Get All Teams
+  get "/teams" do
+    teams = FootballTeam.all.order(:id)
+    teams.to_json
   end
+
+  #Get All Players
+  get "/players" do
+    players = FootballPlayer.all.order(:id)
+    players.to_json
+  end
+
+  #Get Team By ID
+  get "/teams/:id" do
+    team = FootballTeam.find(params[:id])
+    team.to_json
+  end
+
+  #Get Players by Team
+  get "/teams/:id/players" do
+    team = FootballTeam.find(params[:id])
+    players = FootballPlayer.where("football_team_id = ?", team.id)
+    players.to_json
+  end
+
+
+
 
 end
