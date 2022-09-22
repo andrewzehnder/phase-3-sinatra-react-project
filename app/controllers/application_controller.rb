d262c19f446b0442ee01f0a7dd93de053501bbcd
Andrew <azehnder2@gmail.com>
2022-09-22 14:10:01 -0700

    Updating to include players

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,27 +6,15 @@ class ApplicationController < Sinatra::Base
   #Get All Teams
   get '/teams' do
     teams = FootballTeam.all.order(:id)
-    teams.to_json
+    teams.to_json(include: :football_players)
   end
 
-  #Get All Players
-  get '/players' do
-    players = FootballPlayer.all.order(:id)
-    players.to_json
-  end
+  # #Get Team By ID
+  # get '/teams/:id' do
+  #   team = FootballTeam.find(params[:id])
+  #   team.to_json(include: :football_players)
+  # end
 
-  #Get Team By ID
-  get '/teams/:id' do
-    team = FootballTeam.find(params[:id])
-    team.to_json
-  end
-
-  #Get Players by Team
-  get '/teams/:id/players' do
-    team = FootballTeam.find(params[:id])
-    players = FootballPlayer.where('football_team_id = ?', team.id)
-    players.to_json
-  end
 
   #Add New Team
   post '/teams' do
